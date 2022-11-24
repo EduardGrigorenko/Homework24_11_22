1b91f0a1f16a8115eb829a00671564ac651629a8
OMEN <grigorenkoeduard6@gmail.com>
2022-11-24 15:19:07 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner =new Scanner(System.in);
+        int number =scanner.nextInt();
+        int root = 0;
+        while (root < number){
+            System.out.println("Я начинаю разбиратся с циклами ");
+            root ++;
+        }
+
+    }
+}
